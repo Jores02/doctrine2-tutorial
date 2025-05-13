e654bd481c8eee61861e8d9a62ad0cc9ac21d4c2
Amen AHOUANDOGBO <aa290627@iut21.u-bourgogne.fr>
2025-05-13 16:02:58 +0200

    Ajout de l'entité Product

---
+++ src/Product.php
@@ -0,0 +1,22 @@
+<?php
+// src/Product.php
+class Product
+{
+    private int|null $id = null;
+    private string $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
