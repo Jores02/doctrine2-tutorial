637593789684edb1cf6577588909a1ba243d7343
AHOUANDOGBO Amen <112037382+Jores02@users.noreply.github.com>
2025-05-19 20:21:37 +0200

    Entity Repositories

--- src/Bug.php
+++ src/Bug.php
@@ -4,7 +4,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity]
+#[ORM\Entity(repositoryClass: BugRepository::class)]
 #[ORM\Table(name: 'bugs')]
 class Bug
 {
