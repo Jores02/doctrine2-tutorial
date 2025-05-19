265c05fe97017580705d02f70f8a8879c8194ef5
AHOUANDOGBO Amen <112037382+Jores02@users.noreply.github.com>
2025-05-19 20:10:32 +0200

    Nombre de bugs

---
+++ products.php
@@ -0,0 +1,16 @@
+<?php
+// products.php
+
+require_once "bootstrap.php";
+
+$dql = "SELECT p.id, p.name, COUNT(b.id) AS openBugs
+        FROM Bug b
+        JOIN b.products p
+        WHERE b.status = 'OPEN'
+        GROUP BY p.id";
+
+$productBugs = $entityManager->createQuery($dql)->getScalarResult();
+
+foreach ($productBugs as $productBug) {
+    echo $productBug['name'] . " has " . $productBug['openBugs'] . " open bugs!\n";
+}
